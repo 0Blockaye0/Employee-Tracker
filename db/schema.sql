6424771e5bd88d98ac16b60108f1b1c8e8a20b0d
Blake Johnson <blakejohnson@Blakes-MBP.attlocal.net>
2021-05-02 14:55:49 -0500

    initial setup

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30),
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL(6,2),
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL,
+);
+
+
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES department(id) ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+INSERT INTO department (name)
+VALUES 
+    ('Sales')
+    ('Engineering'),
+    ('Marketing'),
+    ('Distribution'),
+    ('Managment');
+
+
+INSERT INTO role (title, salary)
+VALUES
+    ('Salesperon', 45,000.00),
+    ('Sales Lead', 55,500.00),
+    ('Senior Engineer', 120,000.00),
+    ('Junior Engineer', 75,000.00),
+    ('Mareketing Specialist', 55,000.00),
+    ('Marketing Director', 75,000.00),
+    ('Warehouse Crew', 35,500.00),
+    ('Warehouse Manager', 55,000.00);
+
+
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES 
+  ('Ronald', 'Firbank', 1, 1),
+  ('Virginia', 'Woolf', 1, 2),
+  ('Piers', 'Gaveston', 1, 3),
+  ('Charles', 'LeRoi', 2, 4),
+  ('Katherine', 'Mansfield', 2, 5),
+  ('Dora', 'Carrington', 3, 6),
+  ('Edward', 'Bellamy', 3, 7),
+  ('Montague', 'Summers', 3, 7),
+  ('Octavia', 'Butler', 3, 8);  
