c48e0436170b486c2a1a11700a1d5f53dc22dd64
Blake Johnson <blakejohnson@Blakes-MBP.attlocal.net>
2021-05-06 17:57:56 -0500

    initial file structure and db connection file created. schema and seeds created/ seeded without errors.

--- db/schema.sql
+++ db/schema.sql
@@ -23,5 +23,5 @@ CREATE TABLE employee (
   role_id INTEGER,
   manager_id INTEGER NULL,
   CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES department(id) ON DELETE SET NULL
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,33 +1,39 @@
 INSERT INTO department (name)
 VALUES 
-    ('Sales'),
-    ('Engineering'),
-    ('Marketing'),
-    ('Distribution'),
-    ('Managment');
+   ('Sales'),
+   ('Engineering'),
+   ('Marketing'),
+   ('Distribution'),
+   ('Managment');
 
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-    ('Salesperon', 45000.00, 1),
-    ('Sales Lead', 55500.00, 1),
-    ('Senior Engineer', 120000.00, 2),
-    ('Junior Engineer', 75000.00, 2),
-    ('Mareketing Specialist', 55000.00, 3),
-    ('Marketing Director', 75000.00, 3),
-    ('Warehouse Crew', 35500.00, 4),
-    ('Warehouse Manager', 55000.00, 4);
+   ('Salesperon', 45000.00, 1),
+   ('Sales Lead', 55500.00, 1),
+   ('Senior Engineer', 120000.00, 5),
+   ('Junior Engineer', 75000.00, 2),
+   ('Mareketing Specialist', 55000.00, 3),
+   ('Marketing Director', 75000.00, 5),
+   ('Warehouse Crew', 35500.00, 4),
+   ('Warehouse Manager', 55000.00, 5);
+
+
 
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-  ('Ronald', 'Firbank', 1, 1),
-  ('Virginia', 'Woolf', 1, 2),
-  ('Piers', 'Gaveston', 1, 3),
-  ('Charles', 'LeRoi', 2, 4),
-  ('Katherine', 'Mansfield', 2, 5),
-  ('Dora', 'Carrington', 3, 6),
-  ('Edward', 'Bellamy', 3, 7),
-  ('Montague', 'Summers', 3, 7),
-  ('Octavia', 'Butler', 3, 8);  
+    ('Dora', 'Carrington', 3, NULL),
+    ('Katherine', 'Mansfield', 6, NULL),
+    ('Octavia', 'Butler', 8, NULL),
+    ('Ronald', 'Firbank', 1, 2),
+    ('Virginia', 'Woolf', 1, 2),
+    ('Piers', 'Gaveston', 2, 2),
+    ('Charles', 'LeRoi', 4, 1),
+    ('Edward', 'Bellamy', 5, 2),
+    ('Montague', 'Summers', 7, 3),
+    ('James', 'Johnson', 7, 3),
+    ('Levi', 'Winter', 7, 3),
+    ('Pugsley', 'Adams', 7, 3);
+    
